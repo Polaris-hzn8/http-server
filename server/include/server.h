bd5f5cb9a8dc843d8876a77ce873103561e79788
luochenhao <3453851623@qq.com>
2023-04-25 00:48:49 +0800

    feat: finish error page & sendfile

--- server/include/server.h
+++ server/include/server.h
@@ -12,7 +12,6 @@
 
 int acceptClient(int lfd, int epollfd);//与客户端建立连接
 int recvHttpRequest(int cfd, int epollfd);//接受并处理http消息
-int parseHttpRequestLine(const char* line, int cfd);//在接受到http消息之后 进行具体的解析操作
 int epollRun(int lfd);//启动epoll
 
 #endif
